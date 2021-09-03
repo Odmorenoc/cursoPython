a5501d5d6e17362c8317dcc3b4555c7f39ce2ac6
Oscar Moreno <odmc672@gmail.com>
2021-09-03 09:54:03 -0500

    se cambia de nombre el archivo

---
+++ conversor1.py
@@ -0,0 +1,4 @@
+pesos = float(input("ingresa la cantidad de pesos que desea convertir a dolares: "))
+dolar = 3890
+conversion =str(round((pesos / dolar),2))
+print("Tienes $" + conversion + " dolares")
