95dc358632818adea81a58eaa068e1427ec733b5
Oscar Moreno <odmc672@gmail.com>
2021-09-12 18:23:04 -0500

    se crea diccionario.py

---
+++ recursividadBucles/diccionario.py
@@ -0,0 +1,18 @@
+def run():
+    # d={
+    #     1: "hola"+"mundo",
+    #     2: 45,
+    #     3: 75
+    # }
+    # print(d[1])
+    paises={
+        "Argentina": 44938712,
+        "Brazil": 210147125,
+        "Colombia": 50372424
+    }
+    for i,j in paises.items():
+        print(i+" "+ str(j)) 
+
+
+if __name__=="__main__":
+    run()
