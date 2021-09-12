fb29cd72fe4c8561b8a696d31c52e3d8b2e38991
Oscar Moreno <odmc672@gmail.com>
2021-09-12 12:26:15 -0500

    se crea la carpeta recursividadBucles y el archivo recursisvidad.py

--- recursividadBucles/recursividad.py
+++ recursividadBucles/recursividad.py
@@ -3,7 +3,7 @@ def run(rep,cont):
         print("fin!")
     else : 
         print("2^"+ str(cont) + "="+ str(2**cont))
-        potencias(rep,cont+1)
+        run(rep,cont+1)
 
 if __name__=="__main__":
     rep=int(input("¿cuantas repeticiones quiere reealizar? "))
