d20bf394503140a640c6021c68c42583ef7ebe57
Oscar <odmc672@gmail.com>
2021-09-26 17:17:38 -0500

    se cambian los valores de las llaves del diccionario

--- basics2/dictComprehensions.py
+++ basics2/dictComprehensions.py
@@ -1,10 +1,12 @@
-# Se crea una comprensión de un diccionario cuyas llaves sean los primeros 100 N, y los valores sean sus respectivos cubos.
+# Se crea una comprensión de un diccionario cuyas llaves sean los primeros 100 N, y los valores sean los respectivos cubos desde 1 hasta el nuemro de la llave.
 def run():
     dict= {}
-    nums=[i**3 for i in range(1,101)]
+    
     for i in range(1,101):
-        dict[i]=nums
+        dict[i]=[i**3 for i in range(1,i+1)]
+    
     print(dict)
+
 
 if __name__=="__main__":
     run()
