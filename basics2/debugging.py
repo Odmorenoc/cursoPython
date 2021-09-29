3a7036102215a1dd657bf03988dd9d9db8eecf17
Oscar <odmc672@gmail.com>
2021-09-29 15:31:00 -0500

    se corrigen los errores generados por valores invalidos

--- basics2/debugging.py
+++ basics2/debugging.py
@@ -1,16 +1,25 @@
 def divisors(num):
+
     #Se aprende a la depuración de errores con run and debug de VScode.
     #Se crea un breakpoint en la linea 4 en la cual se intuye existe el error.
     #se corrige el error correspondiente al resultado del modulo de la condicion.
+
     divisor=[i for i in range(1,(num+1)) if num%i ==0]
     return divisor
 
 
 def run():
-    num=int(input("ingresa un numero: "))
-    print(divisors(num))
-    print(num)
-    print("Termino el programa")
+
+    # se corrige el error si el usuario ingresa una letra.
+    
+    try:
+        num=int(input(" por favor ingresa un numero: "))
+        print(divisors(num))
+        print(num)
+        print("Termino el programa")
+    except ValueError:
+        print("el valor ingresado no es permitido ingrese solo numeros")
+        run()
     pass
 
 
