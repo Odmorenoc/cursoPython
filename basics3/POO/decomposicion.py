9c7b1932672a95ca388f35911bf6d16876ca5f7a
Oscar <odmc672@gmail.com>
2021-11-15 16:20:15 -0500

    se crean archivos decoradores y decomposición

---
+++ basics3/POO/decomposicion.py
@@ -0,0 +1,34 @@
+def run():
+    pass
+
+class Automovil():
+    
+    def __init__(self, modelo, marca, color):
+        self.modelo = modelo
+        self.marca = marca
+        self.color = color
+        self._estado = 'en reposo'
+        self._motor = Motor(4)
+    
+    def acelerar(self, tipo='despacio'):
+        if tipo == 'rapido':
+            self._motor.inyecta_gasolina(10)
+        else:
+            self._motor.inyecta_gasolina(3)
+        
+        self._estado = 'en movimiento'
+    pass
+
+class Motor:
+
+    def __init__(self, cilindros, tipo='gasolina'):
+        self.cilindros = cilindros
+        self.tipo = tipo
+        self._temperatura = 0
+
+    def inyecta_gasolina(self, cantidad):
+        self.cantidad = 5
+
+
+if __name__=='__main__':
+    run()
