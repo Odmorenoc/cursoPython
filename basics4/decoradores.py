3cbf811f31b31e22642df28b4bd83fe4e2c8c4d9
Oscar <odmc672@gmail.com>
2022-01-05 19:15:35 -0500

    se crea decoradores.py

---
+++ basics4/decoradores.py
@@ -0,0 +1,21 @@
+def mayusculas(funcion):
+    def envoltura(texto):
+        return funcion(texto).upper()
+    return envoltura
+
+def mensajePersonalizado(mensaje):
+    def conMensaje(funcion):
+        print(f'{mensaje}')
+        def envoltura(*args, **kwargs):
+            resultado = funcion(*args, **kwargs)
+            return resultado
+        return envoltura 
+    return conMensaje
+
+
+@mayusculas
+@mensajePersonalizado(f'hola ')
+def mensaje(nombre):
+    return print(f'{nombre}, recibiste un mensaje') 
+
+mensaje('oscar')
